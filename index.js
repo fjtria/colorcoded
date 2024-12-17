6cd1a3bcd6068d475c9bb2d8769050b8e9de1526
FJ Tria <fjstria@gmail.com>
2024-12-16 23:12:17 -0800

    Text effects

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// Hacked Text Effect: https://codepen.io/Hyperplexed/full/rNrJgrd
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
+document.querySelector("#hacked").onmouseover = event => {  
+    let iteration = 0;  
+    clearInterval(interval);
+    interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+            if(index < iteration) {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+        if(iteration >= event.target.dataset.value.length){ 
+            clearInterval(interval);
+        }
+        iteration += 1 / 3;
+    }, 35);
+}
+
 // DOM Elements
 const dropdown = document.getElementById("input-type");
 const inputField = document.getElementById("input");
